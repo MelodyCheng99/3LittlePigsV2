a4e98cf2706be520ee42edbbe8e13763d72d2aec
Melody Cheng <melodycheng99@gmail.com>
2020-08-09 16:49:18 -0400

    Get game code on frontend from backend

---
+++ server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import random
+import string
+
+app = Flask(__name__)
+
+@app.route('/create-game')
+def create_game():
+    return {
+        'gameCode': ''.join((random.choice(string.ascii_letters + string.digits) for i in range(5)))
+    }
+    
--- src/index.js
+++ src/index.js
@@ -4,10 +4,26 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
+import rootSaga from './sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware),
+);
+
+sagaMiddleware.run(rootSaga);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
