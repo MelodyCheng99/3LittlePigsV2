19a1215027cf7d324e6b66310dc7c73bb7b761c2
Melody Cheng <melodycheng99@gmail.com>
2020-09-04 11:51:17 -0400

    Change placement of code

---
+++ client/build/precache-manifest.c071b0689626b386c9e756c4b2d449ae.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d08e6159ff82a85cf5e1468c4f696469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19a67e9526179f5eed78",
+    "url": "/static/css/main.4bf3af07.chunk.css"
+  },
+  {
+    "revision": "066501fd4fd964ab296d",
+    "url": "/static/js/2.685a496d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.685a496d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a67e9526179f5eed78",
+    "url": "/static/js/main.342af197.chunk.js"
+  },
+  {
+    "revision": "e01f10112338d75feacb",
+    "url": "/static/js/runtime-main.dd7c2c79.js"
+  },
+  {
+    "revision": "65b86e3ce838736fb1e3ea4be28395af",
+    "url": "/static/media/brick.65b86e3c.png"
+  },
+  {
+    "revision": "d6fc43dabd59c2580266f42427d65e50",
+    "url": "/static/media/brick_icon.d6fc43da.png"
+  },
+  {
+    "revision": "652834032c10ce1b724c35c70e333876",
+    "url": "/static/media/brick_or_mud.65283403.png"
+  },
+  {
+    "revision": "72bee46b887897d49527bcffd338ea73",
+    "url": "/static/media/flower.72bee46b.png"
+  },
+  {
+    "revision": "1218ba3539659bc84c7bc598a1dfefd9",
+    "url": "/static/media/mud.1218ba35.png"
+  },
+  {
+    "revision": "68d33d034a154ec9f7e239fa4b1d6348",
+    "url": "/static/media/mud_icon.68d33d03.png"
+  },
+  {
+    "revision": "03075c872470b5bd01f59eaf6d530c34",
+    "url": "/static/media/stick.03075c87.png"
+  },
+  {
+    "revision": "25988e403f51e3e2b41bd8b138ae68ae",
+    "url": "/static/media/stick_icon.25988e40.png"
+  },
+  {
+    "revision": "99835989e58db8e2ad148d9239e75473",
+    "url": "/static/media/stone.99835989.png"
+  },
+  {
+    "revision": "7e3ed856f14284b17415a8c6f8dbbae6",
+    "url": "/static/media/stone_or_stick.7e3ed856.png"
+  },
+  {
+    "revision": "18d8077ada15bda1f1db34b12646f62f",
+    "url": "/static/media/vp_3.18d8077a.png"
+  },
+  {
+    "revision": "8f8dde524dd4a1c83ae5dc63c5f1c809",
+    "url": "/static/media/vp_5.8f8dde52.png"
+  },
+  {
+    "revision": "27cf558d555be2f21259213380de4f60",
+    "url": "/static/media/vp_7.27cf558d.png"
+  }
+]);
--- server/server.py
+++ server/server.py
@@ -1,15 +1,11 @@
-from flask import Flask, request
+from flask import Flask
 
 import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import db
-
-import random
-import string
 
 app = Flask(__name__)
 
-cred = credentials.Certificate('../threelittlepigsv2-b4ec708c9fff.json')
+cred = credentials.Certificate('./threelittlepigsv2-b4ec708c9fff.json')
 firebase_admin.initialize_app(
     cred,
     {
@@ -17,60 +13,4 @@ firebase_admin.initialize_app(
     }
 )
 
-@app.route('/create-game')
-def create_game():
-    # TODO: Ensure gameCode doesn't already exist in database
-    return {
-        'gameCode': ''.join((random.choice(string.ascii_letters + string.digits) for i in range(5)))
-    }
-
-@app.route('/create-player', methods=['POST'])
-def create_player():
-    gameCode = request.form.get('game_code')
-    username = request.form.get('username')
-
-    gameResult = db.reference('games/' + gameCode).get()
-
-    boardsResult = db.reference('boards').get()
-    boardsResult.pop(0)
-
-    cardsResult = db.reference('cards').get()
-    cardsResult.pop(0)
-
-    if gameResult != None:
-        for player in gameResult.values():
-            boardsResult.remove(player['board'])
-            for card in player['cards']:
-                cardsResult.remove(card)
-
-    board = random.sample(boardsResult, 1)[0]
-    cards = random.sample(cardsResult, 7)
-
-    stats = {
-        'Coins': 3,
-        'Bricks': 0,
-        'Sticks': 0,
-        'Mud': 0,
-        'Stones': 0,
-        'Wolves': 0,
-        'Pots': 0,
-        'Spoons': 0,
-        'Apples': 0,
-        'Flowers': 0,
-        'Glass': 0,
-        'Water': 0,
-        'Victory Points': 0
-    }
-
-    result = {
-        'board': board,
-        'cards': cards,
-        'stats': stats
-    }
-
-    if gameResult != None:
-        db.reference('games').child(gameCode).update({ username: result })
-    else:
-        db.reference('games').child(gameCode).set({ username: result })
-
-    return result
+import registration
