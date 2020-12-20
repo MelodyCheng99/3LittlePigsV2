060583afb3a2e8c698d71018f570be7f827ace30
Melody Cheng <melodycheng99@gmail.com>
2020-12-20 11:55:58 -0500

    Force restarting of project

--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import CreateOrJoinGame from './components/CreateOrJoinGame/createOrJoinGameComponent';
-
-export default class App extends React.Component {
-  render() {
-    return <CreateOrJoinGame />
-  }
-}
